c1c885e5ceac5581270aa758815954910fc77e5f
Russell Boley <raboley@gmail.com>
2020-10-26 20:03:35 -0700

    Navigator now will use pathfinder.

--- Pathfinder.Tests/UnitTests/NavigatorTests.cs
+++ Pathfinder.Tests/UnitTests/NavigatorTests.cs
@@ -6,23 +6,31 @@ namespace Pathfinder.Tests.UnitTests
     public class NavigatorTests
     {
         [Fact]
-        public void NavigatorMoveToWaypointMovesPlayer()
+        public void NavigatorFindsPathToWaypoint()
         {
-            Vector3[] path = new[]
+            Vector3[] want =
             {
-                new Vector3(-1f),
-                new Vector3(0f),
-                new Vector3(1f),
+                new Vector3(1f, 0f, 1f),
             };
+            var goal = new Vector3(2f);
+
+            var grid = GridSetup.SetupMediumGrid();
+
+            var pathfinding = new Pathfinding();
+            pathfinding.Grid = grid;
 
             var navigator = new Navigator();
+            navigator.Position = new Vector3(-2f);
+            navigator.Pathfinder = pathfinding;
 
-            var traveledPath = navigator.WalkPath(path);
+            var traveledPath = navigator.WalkToWaypoint(goal);
 
-            Assert.Equal(path.Length, traveledPath.Length);
-            for (int i = 0; i < path.Length; i++)
+
+            Assert.Equal(goal, navigator.Position);
+            Assert.Equal(want.Length, traveledPath.Length);
+            for (int i = 0; i < want.Length; i++)
             {
-                Assert.Equal(path[i], traveledPath[i]);
+                Assert.Equal(want[i], traveledPath[i]);
             }
         }
     }
--- Pathfinder/Navigator.cs
+++ Pathfinder/Navigator.cs
@@ -4,9 +4,14 @@ namespace Pathfinder
 {
     public class Navigator
     {
-        public Vector3[] WalkPath(Vector3[] path)
+        public Vector3[] WalkToWaypoint(Vector3 waypoint)
         {
-            return path;
+            var findWaypoints = Pathfinder.FindWaypoints(Position, waypoint);
+            Position = waypoint;
+            return findWaypoints;
         }
+
+        public Pathfinding Pathfinder { get; set; }
+        public Vector3 Position { get; set; }
     }
 }
