efb69d67ffc583507054668e4c54c4441053380e
Russell Boley <raboley@gmail.com>
2020-11-16 19:22:40 -0800

    Added a test for zome maps.

---
+++ Pathfinder.Tests/IntegrationTests/ZonePersisterTests.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using Pathfinder.Map;
+using Xunit;
+
+namespace Pathfinder.Tests.IntegrationTests
+{
+    public class ZonePersisterTests
+    {
+        [Fact]
+        public void FilePersisterCanSaveAndLoadZone()
+        {
+            var want = World.FinalFantasy().Zones.FirstOrDefault();
+            var persister = SetupPersister.SetupTestFilePersister();
+            string mapName = SetupPersister.GetCurrentMethodName();
+            persister.FileName = mapName;
+
+            persister.Save(want);
+            var got = persister.Load<Zone>();
+
+            Assert.Equal(want, got);
+        }
+    }
+}
